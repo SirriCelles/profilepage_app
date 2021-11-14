5f0a27233a78ba718fd3c0623b46ca2337e37a4a
sirricelles <sirricelles@gmail.com>
2021-11-14 10:28:15 +0100

    partial template added with routing module

--- app/controller.js
+++ app/controller.js
@@ -1,10 +1,15 @@
 var profilePageApp = angular
                         .module("profileModule", [])
-                        .controller("profileController", function ($scope) {
+                        .controller("profileController", function ($scope, $location, $anchorScroll) {
                             var images = "assets/images";
                             var title = "Profile Page";
 
                             $scope.images = images;
                             $scope.title = title;
-                            
+                            $scope.homePage = "home-page.html";
+                            $scope.aboutPage = "about-page.html";
+                            $scope.scrollTo = function (pageLocation) {
+                                $location.hash(pageLocation);
+                                $anchorScroll();
+                            };
                         });
